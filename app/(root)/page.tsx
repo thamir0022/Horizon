b60305f97aa43ff713a6254abbc46b0e40834089
Thamir <thamirsiddik@gmail.com>
2024-05-17 18:03:24 +0530

    Implemented doughnut chart, available bank balance.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,27 @@
+import HeaderBox from "../../components/HeaderBox";
+import TotalBalanceBox from "../../components/TotalBalanceBox";
+
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    );
-}
+  const loggedIn = { firstName: "Thamir" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
 
 export default Home;
