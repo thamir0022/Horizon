8c414289917e73f49321456dd3945ec0d9f882e7
Thamir <thamirpkd004@gmail.com>
2024-06-12 21:29:44 +0530

    Implement Plaid link and Dwolla

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -23,6 +23,7 @@ import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
 import { signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
@@ -31,91 +32,91 @@ const AuthForm = ({ type }: { type: string }) => {
 
   const formSchema = authFormSchema(type);
 
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        email: "",
-        password: ''
-      },
-    })
-   
-    // 2. Define a submit handler.
-    const onSubmit = async (data: z.infer<typeof formSchema>) => {
-      setIsLoading(true);
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: ''
+    },
+  })
 
-      try {
-        // Sign up with Appwrite & create plaid token
-        
-        if(type === 'sign-up') {
-          const userData = {
-            firstName: data.firstName!,
-            lastName: data.lastName!,
-            address1: data.address1!,
-            city: data.city!,
-            state: data.state!,
-            postalCode: data.postalCode!,
-            dateOfBirth: data.dateOfBirth!,
-            ssn: data.ssn!,
-            email: data.email,
-            password: data.password
-          }
+  // 2. Define a submit handler.
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
 
-          const newUser = await signUp(userData);
+    try {
+      // Sign up with Appwrite & create plaid token
 
-          setUser(newUser);
+      if (type === 'sign-up') {
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
         }
 
-        if(type === 'sign-in') {
-          const response = await signIn({
-            email: data.email,
-            password: data.password,
-          })
+        const newUser = await signUp(userData);
 
-          if(response) router.push('/')
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
+        setUser(newUser);
       }
+
+      if (type === 'sign-in') {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        })
+
+        if (response) router.push('/')
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
     }
+  }
 
   return (
     <section className="auth-form">
       <header className='flex flex-col gap-5 md:gap-8'>
-          <Link href="/" className="cursor-pointer flex items-center gap-1">
-            <Image 
-              src="/icons/logo.svg"
-              width={34}
-              height={34}
-              alt="Horizon logo"
-            />
-            <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
-          </Link>
+        <Link href="/" className="cursor-pointer flex items-center gap-1">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Horizon logo"
+          />
+          <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
+        </Link>
 
-          <div className="flex flex-col gap-1 md:gap-3">
-            <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
-              {user 
-                ? 'Link Account'
-                : type === 'sign-in'
-                  ? 'Sign In'
-                  : 'Sign Up'
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {user
+              ? 'Link Account'
+              : type === 'sign-in'
+                ? 'Sign In'
+                : 'Sign Up'
+            }
+            <p className="text-16 font-normal text-gray-600">
+              {user
+                ? 'Link your account to get started'
+                : 'Please enter your details'
               }
-              <p className="text-16 font-normal text-gray-600">
-                {user 
-                  ? 'Link your account to get started'
-                  : 'Please enter your details'
-                }
-              </p>  
-            </h1>
-          </div>
+            </p>
+          </h1>
+        </div>
       </header>
       {user ? (
         <div className="flex flex-col gap-4">
-          {/* <PlaidLink user={user} variant="primary" /> */}
+          <PlaidLink user={user} variant="primary" />
         </div>
-      ): (
+      ) : (
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -149,7 +150,7 @@ const AuthForm = ({ type }: { type: string }) => {
                       <Loader2 size={20} className="animate-spin" /> &nbsp;
                       Loading...
                     </>
-                  ) : type === 'sign-in' 
+                  ) : type === 'sign-in'
                     ? 'Sign In' : 'Sign Up'}
                 </Button>
               </div>
@@ -159,8 +160,8 @@ const AuthForm = ({ type }: { type: string }) => {
           <footer className="flex justify-center gap-1">
             <p className="text-14 font-normal text-gray-600">
               {type === 'sign-in'
-              ? "Don't have an account?"
-              : "Already have an account?"}
+                ? "Don't have an account?"
+                : "Already have an account?"}
             </p>
             <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className="form-link">
               {type === 'sign-in' ? 'Sign up' : 'Sign in'}
---
+++ components/PlaidLink.tsx
@@ -0,0 +1,45 @@
+import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from "react-plaid-link";
+import { Button } from "./ui/button";
+import { useCallback, useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { createLinkToken, exchangePublicToken } from "@/lib/actions/user.actions";
+
+const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
+    const router = useRouter();
+    const [token, setToken] = useState('');
+    const onSuccess = useCallback<PlaidLinkOnSuccess>(async (public_token: string) => {
+        await exchangePublicToken({
+            publicToken: public_token,
+            user,
+        });
+        router.push('/');
+    }, [user]);
+
+    useEffect(() => {
+        const getLinkToken = async () => {
+           const data = await createLinkToken(user);
+        setToken(data?.linkToken);
+        }
+        getLinkToken();
+    }, []);
+
+    const config: PlaidLinkOptions = {
+        token,
+        onSuccess
+    }
+
+    const {open, ready} = usePlaidLink(config);
+    return (
+        <>
+            {variant === 'primary' ? (
+                <Button onClick={() => open()} disabled={!ready} className="plaidlink-primary">Connect Bank</Button>
+            ) : variant === 'ghost' ? (
+                <Button>Connect Bank</Button>
+            ) : (
+                <Button>Connect Bank</Button>
+            )}
+        </>
+    );
+}
+
+export default PlaidLink;
